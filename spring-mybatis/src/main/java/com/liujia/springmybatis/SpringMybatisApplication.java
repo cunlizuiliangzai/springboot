3bbee1747d3f1af89a1645881f4a450401b91c98
liujia <hctcs340@126.com>
2020-11-13 13:53:01 +0800

    mybatis

--- spring-mybatis/src/main/java/com/liujia/springmybatis/SpringMybatisApplication.java
+++ spring-mybatis/src/main/java/com/liujia/springmybatis/SpringMybatisApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMybatisApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringMybatisApplication.class, args);
     }
 
