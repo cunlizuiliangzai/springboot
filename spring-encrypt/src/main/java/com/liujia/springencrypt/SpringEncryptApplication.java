fed97f3e43ed221beda63278d5904d3c4ffee111
liujia <hctcs340@126.com>
2020-11-24 17:05:47 +0800

    commit

---
+++ spring-encrypt/src/main/java/com/liujia/springencrypt/SpringEncryptApplication.java
@@ -0,0 +1,13 @@
+package com.liujia.springencrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEncryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEncryptApplication.class, args);
+    }
+
+}
---
+++ spring-encrypt/src/test/java/com/liujia/springencrypt/SpringEncryptApplicationTests.java
@@ -0,0 +1,8 @@
+package com.liujia.springencrypt;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEncryptApplicationTests {
+
+}
